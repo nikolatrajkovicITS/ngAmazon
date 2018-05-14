71e95108cd25a2a2381d58ff50d43fb0242b73fe
NikolaTrajkovic <nikolatrajkovic.contact@gmail.com>
2018-05-14 20:01:43 +0200

    Implemented navbar for mobile device and routing

--- amazon/src/app/app-routing.module.ts
+++ amazon/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- amazon/src/app/app.module.ts
+++ amazon/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
