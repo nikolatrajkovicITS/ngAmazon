c0b60bbce40cb5834c5e346146a3a2eaaf6c453c
NikolaTrajkovic <nikolatrajkovic.contact@gmail.com>
2018-05-17 21:07:47 +0200

    Created UserSchema and hash password function

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bcrypt = require('bcrypt-nodejs');
+
+const UserSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true }, 
+    name: String,
+    password: String,
+    picture: String,
+    isSeller: { type: Boolean, default: false },
+    address: {
+        addr1: String,
+        addr2: String,
+        city: String,
+        state: String,
+        country: String,
+        postalCode: String
+    },
+    created: { type: Date, default: Date.now }
+});
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+    });
+});
+
